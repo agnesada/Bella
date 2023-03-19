1144fc1a00684404ee4fd1395fcd5d061bbd01bb
Agnes Ada Aba <agnes.aba15@gmail.com>
2023-03-19 08:26:13 +0100

    new

--- src/View.js
+++ src/View.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Navbar from "./Pages/Navigation/Nav" 
-import Hero_Section from "./Pages/HeroSection/hero"
+import HeroSection from "./Pages/HeroSection/hero"
 import Features from "./Pages/FeaturesSection/features"
-import CTA_Footer from "./Pages/CTAfooter/cta"
-import Footer_Section from "./Pages/FooterSection/footer"
+import CTAFooter from "./Pages/CTAfooter/cta"
+import FooterSection from "./Pages/FooterSection/footer"
 
 
 const View = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Hero_Section></Hero_Section>
+      <HeroSection></HeroSection>
       <Features></Features>
-      <CTA_Footer></CTA_Footer>
-      <Footer_Section></Footer_Section>
+      <CTAFooter></CTAFooter>
+      <FooterSection></FooterSection>
     </div>
   )
 }
